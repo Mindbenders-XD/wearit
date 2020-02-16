cc6ce3805254d56318d6bb1fd73ae2d23abe64d2
Janakiraman <Janakiramanc@users.noreply.github.com>
2020-02-16 10:08:18 +0530

    Update header.js

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -34,7 +34,7 @@ function Header(props){
                             <a href="#/">Shop</a>
                             <a href="#/">Blog</a>
                             <a href="#/">Pages</a>
-                            <a href="#/">Contact Us</a>
+                            <a href="#/">Contact</a>
                         </div>
                         <div className="header__icons">
                             <a href="#/">
