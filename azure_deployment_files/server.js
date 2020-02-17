19325e8cf56cc62e66fe8822c37051b516ed570a
AnandiY <anandi.yogeesan@gmail.com>
2020-02-17 15:23:57 +0530

    Deployment Package.json updated

--- azure_deployment_files/server.js
+++ azure_deployment_files/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-let port = process.env.PORT || 2000;
+let port = process.env.PORT || 9000;
 app.use(express.static(path.join(__dirname)));
 
 app.get('/ping', function (req, res) {
