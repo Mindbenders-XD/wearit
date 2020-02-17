b3d53e8d7b6ee63998ea19df67acae8453095b49
AnandiY <anandi.yogeesan@gmail.com>
2020-02-17 12:08:21 +0530

    Update banner.js

--- src/components/productCatalog/banner.js
+++ src/components/productCatalog/banner.js
@@ -6,7 +6,7 @@ function Banner(){
         <div className="banner__overlay"></div>
         <div className="banner__content">
             <h1 className="banner__title">Fashion for <br/> Upcoming Winter</h1>
-            <p className="banner__description">Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+            <p className="banner__description">Wear It is a fashion apparael brand. It caters to all demographics. From a new born baby, kids, youngsters and all have something to take away.</p>
             <button type="button" className="btn btn__big btn--bold  btn--white" aria-label="Center Align">View Collection</button>
         </div>
     </section>
